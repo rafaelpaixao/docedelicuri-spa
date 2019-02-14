75107ab70d55809c113404a963cb8c01ef01ec2c
Rafael <rafaelsspaixao@gmail.com>
2019-02-13 23:49:00 -0300

    remove roboto

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from "vue";
-import './plugins/vuetify'
-import './plugins/analytics'
-import './plugins/globals'
+import "./plugins/vuetify";
+import "./plugins/analytics";
+import "./plugins/globals";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.config.productionTip = false;
 
